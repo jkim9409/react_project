5840486bf9456f4911a494b47a156db8f2215c8e
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-08-25 20:02:27 +0900

    add components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
-import './App.css';
+import "./style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          First React Project letsgo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BucketList from './BucketList';
+// import {BucketList} from "./BucketList";
+
+
+
+import LifecycleEx from './LifecycleEx';
+import React from 'react';
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BucketList/>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      list: ["영화관 가기", "매일 책읽기", "수영배우기"],
+    }
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>내 버킷리스트</h1>
+          <hr className="line"/>
+          <BucketList here={this.state.list}/>
+        </div>
+      </div>
+    )
+  }
+
+
+
 }
 
 export default App;
